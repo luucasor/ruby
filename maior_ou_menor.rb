1ac1e5b53b19ea4a4a2417d5258f5b3fb20c0822
luucasor <luucasor@gmail.com>
2016-10-28 11:02:42 -0200

    entrada e saida de dados

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,10 +1,16 @@
 puts "Bem vindo ao jogo da adivinhação"
 puts "Qual seu nome?"
 nome = gets
-
-puts
-puts
-puts
-puts
+numero_secreto = 175
+puts "\n\n\n\n"
 
 puts "Começaremos o jogo pra você, " + nome
+puts "Escolhendo um número secreto entre 0 e 200..."
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+puts "\n"
+
+puts "Tentativa 1"
+puts "Entre com o número"
+chute = gets
+puts "Será que você acertou? Você chutou " + chute
+puts chute.to_i == numero_secreto
