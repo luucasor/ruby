1b3f83fdb135080527273b7fdd2a21c32219e26f
luucasor <luucasor@gmail.com>
2016-11-03 19:30:17 -0200

    condição;laços;comentario

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -7,10 +7,28 @@ puts "\n\n\n\n"
 puts "Começaremos o jogo pra você, " + nome
 puts "Escolhendo um número secreto entre 0 e 200..."
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-puts "\n"
 
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que você acertou? Você chutou " + chute
-puts chute.to_i == numero_secreto
+limite_tentativas = 3
+
+for tentativa in 1..limite_tentativas
+  puts "\n"
+  puts "Tentativa " + tentativa.to_s + " de " + limite_tentativas.to_s
+  puts "Entre com o número"
+  chute = gets
+  puts "Será que você acertou? Você chutou " + chute
+  acertou = chute.to_i == numero_secreto
+
+  # comentario ruby
+
+  if acertou
+      puts "Acertou!"
+      break
+    else
+      maior = numero_secreto > chute.to_i
+      if maior
+        puts "O numero secreto é maior!"
+      else
+        puts "O numero secreto é menor!"
+      end
+  end
+end
--- teste.rb
+++ teste.rb
@@ -1,2 +1,7 @@
 puts 175 == 175
 puts 175 == 174
+
+puts "\n"
+for dedos in 1..3
+  puts "Contando " + dedos.to_s
+end
